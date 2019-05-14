c5d4f6abb37db7cc6046320d50bb0415f562fb60
haduki1208 <syo4kante2@gmail.com>
2019-05-14 13:47:37 +0900

    singletonパターン書いてみる

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # isc-design-pattern-kadai
+
+学校の課題
+Java じゃなくて JS・Python で書かせてくださいお願いします
+
+1. singleton
---
+++ singleton/main.py
@@ -0,0 +1,39 @@
+class Renban:
+    _instance = None
+    _number = 1
+
+    def __new__(self):
+        if self._instance is None:
+            self._instance = object.__new__(self)
+        return self._instance
+
+    @classmethod
+    def get_instance(self):
+        if self._instance is None:
+            self._instance = Renban()
+        return self._instance
+
+    def getNumber(self) -> str:
+        numStr = str(self._number)
+        self._number += 1
+        return numStr.zfill(4)
+
+
+class SingletonTest:
+    @classmethod
+    def main(self):
+        renban = Renban.get_instance()
+        print(renban.getNumber())
+
+        renban = Renban.get_instance()
+        print(renban.getNumber())
+
+        renban = Renban()
+        print(renban.getNumber())
+
+        renban = Renban()
+        print(renban.getNumber())
+
+
+if __name__ == "__main__":
+    SingletonTest.main()
