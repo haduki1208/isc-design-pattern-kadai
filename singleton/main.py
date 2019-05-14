85c14a93aaa03f10b8766bc4953e178d7381c7a0
haduki1208 <syo4kante2@gmail.com>
2019-05-14 13:55:13 +0900

    __new__と__init__の挙動の違いを把握

--- singleton/main.py
+++ singleton/main.py
@@ -2,10 +2,19 @@ class Renban:
     _instance = None
     _number = 1
 
-    def __new__(self):
+    """
+    __new__ は コンストラクタ、get_instanceでシングルトンできる
+    __init__ はget_instanceでシングルトンできる
+    """
+
+    def __init__(self):
         if self._instance is None:
-            self._instance = object.__new__(self)
+            self._instance = object.__init__(self)
         return self._instance
+    # def __new__(self):
+    #     if self._instance is None:
+    #         self._instance = object.__new__(self)
+    #     return self._instance
 
     @classmethod
     def get_instance(self):
@@ -22,17 +31,22 @@ class Renban:
 class SingletonTest:
     @classmethod
     def main(self):
+        """
+        __new__ でシングルトンする場合、以下のコードは正常に動作する
+        __init__ でシングルトンする場合、以下のコードはエラーを吐く
+        """
         renban = Renban.get_instance()
         print(renban.getNumber())
-
         renban = Renban.get_instance()
         print(renban.getNumber())
-
-        renban = Renban()
+        renban = Renban.get_instance()
         print(renban.getNumber())
-
-        renban = Renban()
-        print(renban.getNumber())
+        # renban = Renban()
+        # print(renban.getNumber())
+        # renban = Renban()
+        # print(renban.getNumber())
+        # renban = Renban()
+        # print(renban.getNumber())
 
 
 if __name__ == "__main__":
